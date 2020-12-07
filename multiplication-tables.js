318e59ef25735f869d75a035a206e21a16b65f24
Jón Jonker <66784287+jonjonker@users.noreply.github.com>
2020-12-07 22:44:49 +0100

    Add files via upload

---
+++ multiplication-tables.js
@@ -0,0 +1,7 @@
+let multiplier = 0
+
+for (let num= multiplier; multiplier <=10; multiplier++)
+    for (let num = 1; num <=10; num++) {
+        total = num*multiplier;
+        console.log(num + " x " + multiplier + " = " + total);
+    }
